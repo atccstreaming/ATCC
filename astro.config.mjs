5e5b2f3a2414efbb99fa439a9f6816e93e600d44
Hong Zeng <zengh@wwt.com>
2026-02-24 11:05:32 -0500

    Use www custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://agapetrianglechinesechurch.org',
+  site: 'https://www.agapetrianglechinesechurch.org',
   base: '/',
   integrations: [tailwind()],
 });
